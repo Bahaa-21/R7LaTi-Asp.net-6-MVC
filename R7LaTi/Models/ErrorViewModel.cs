486543e6cf1a78c2de17e70e98f850478b97dd3c
Bahaa Al_Deen <bahaa.ate16@gmail.com>
2022-10-06 11:45:58 +0300

    A new project in asp.net core mvc

---
+++ R7LaTi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace R7LaTi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
